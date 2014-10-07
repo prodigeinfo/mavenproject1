787a9f555838de910ac998a989c273de6f14f640
DMA7746 <stephane.russell@desjardins.com>
2014-10-07 13:25:50 -0400

    Creation du depot initial

---
+++ src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author as000rt
+ */
+public class NewClass {
+    
+    static public int main(String args[]) {
+        System.out.println("Bonjour!");
+        
+        return 0;
+    }
+    
+}
